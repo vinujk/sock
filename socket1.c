68f03a6d5849c0ccc575332b0fca96d1dab68be9
vinujk <vinu.jkataraki@gmail.com>
2025-03-03 08:24:47 -0500

    Add files via upload

--- socket1.c
+++ socket1.c
@@ -5,8 +5,9 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <netinet/ip.h>
+#include "socket.h"
 
-#define RSVP_PROTOCOL 46  // RSVP IP Protocol Number
+/*#define RSVP_PROTOCOL 46  // RSVP IP Protocol Number
 #define PATH_MSG_TYPE 1   // RSVP-TE PATH Message Type
 #define RESV_MSG_TYPE 2   // RSVP-TE RESV Message Type
 
@@ -32,24 +33,52 @@ struct rsvp_header {
     struct in_addr receiver_ip;
 };
 
-// Label Object for RESV Message
+// common class Object 
 struct class_obj {
     uint8_t class_num;
     uint8_t c_type;
     uint16_t length;
 };
 
+// Label Object for PATH Message
 struct label_req_object {
-    struct class_obj *class_obj;
+    struct class_obj class_obj;
     uint16_t Reserved;
     uint16_t L3PID;
 };
+
+// Label Object for PATH Message
+struct session_object {
+    struct class_obj *class_obj;
+    uint32_t ipv4_tunnel_dest_addr;
+    uint16_t Reserved;
+    uint16_t Tunnel_ID;
+    uint16_t Extended_tunnel_ID;
+};  
+
+// Label Object for PATH Message
+struct session_attr {
+    struct class_obj *class_obj;
+    uint8_t setup_prio;
+    uint8_t holding_prio;
+    uint8_t flags;
+    uint8_t Name_len;
+    char Name[32];
+}
+
+// Label Object for PATH Message
+struct Sender_template_object {
+    struct class_obj *class_obj;
+    uint32_t ipv4_tunnel_src_addr;  
+    uint16_t Reserved;
+    uint16_t LSP_ID;
+}
 
 // Label Object for RESV Message
 struct label_object {
     struct class_obj *class_obj;
     uint32_t label;
-};
+};*/
 
 // Function to send an RSVP-TE PATH message
 void send_path_message(int sock, struct in_addr sender_ip, struct in_addr receiver_ip) {
@@ -57,8 +86,11 @@ void send_path_message(int sock, struct in_addr sender_ip, struct in_addr receiv
     char path_packet[64];
 
     struct rsvp_header *path = (struct rsvp_header*)path_packet;
-    struct class_obj *class_obj = (struct class_obj*)(path_packet + sizeof(struct rsvp_header));
-    struct label_req_object *label_req_obj = (struct label_req_object*)(path_packet + sizeof(struct rsvp_header) + sizeof(struct label_req_object));
+    //struct class_obj *class_obj = (struct class_obj*)(path_packet + START_SENT_CLASS_OBJ); 
+    struct session_object *session_obj = (struct session_object*)(path_packet + START_SENT_SESSION_OBJ); 
+    struct label_req_object *label_req_obj = (struct label_req_object*)(path_packet + START_SENT_LABEL_REQ); 
+    struct session_attr_object *session_attr_obj = (struct session_attr_object*)(path_packet + START_SENT_SESSION_ATTR_OBJ); 
+    struct sender_temp_object *sender_temp_obj = (struct sender_temp_object*)(path_packet + START_SENT_SENDER_TEMP_OBJ);
 
     // Populate RSVP PATH header
     path->version_flags = 0x10;  // RSVP v1
@@ -70,12 +102,40 @@ void send_path_message(int sock, struct in_addr sender_ip, struct in_addr receiv
     path->sender_ip = sender_ip;
     path->receiver_ip = receiver_ip;
 
-    printf(" sending message\n");
-    // Populate Label Object                                                      
-    class_obj->class_num = 19;  // Label Request class                                    
-    class_obj->c_type = 1;  // Generic Label                                      
-    class_obj->length = htons(sizeof(struct label_req_object));                       
+    //session object for PATH msg
+    session_obj->class_obj.class_num = 1;
+    session_obj->class_obj.c_type = 7;
+    session_obj->class_obj.length = htons(sizeof(struct session_object));
+    session_obj->dst_ip = receiver_ip;
+    //inet_pton(AF_INET, receiver_ip, &session_obj->dst_ip); 
+    session_obj->tunnel_id = 1;
+    session_obj->src_ip = sender_ip;
+    //inet_pton(AF_INET, sender_ip, &session_obj->src_ip);
+
+    // Populate Label Object                                        
+    label_req_obj->class_obj.class_num = 19;  // Label Request class
+    label_req_obj->class_obj.c_type = 1;  // Generic Label                   
+    label_req_obj->class_obj.length = htons(sizeof(struct label_req_object));
     label_req_obj->L3PID = htonl(0x0800);  // Assigned Label (1001)
+
+    //session attribute object for PATH msg
+    session_attr_obj->class_obj.class_num = 207;
+    session_attr_obj->class_obj.c_type = 1;
+    session_attr_obj->class_obj.length = htons(sizeof(struct session_attr_object));
+    session_attr_obj->setup_prio = 7;
+    session_attr_obj->hold_prio = 7;
+    session_attr_obj->flags = 0;
+    session_attr_obj->name_len = sizeof("PE1");
+    strcpy("PE1", session_attr_obj->Name);
+    
+    //Sender template object for PATH msg
+    sender_temp_obj->class_obj.class_num = 11;
+    sender_temp_obj->class_obj.c_type = 7;
+    sender_temp_obj->class_obj.length = htons(sizeof(struct sender_temp_object));    
+    //inet_pton(AF_INET, sender_ip, &sender_temp_obj->src_ip);
+    sender_temp_obj->src_ip = sender_ip;
+    sender_temp_obj->Reserved = 0;
+    sender_temp_obj->LSP_ID = 2;
 
     // Set destination (egress router)
     dest_addr.sin_family = AF_INET;
