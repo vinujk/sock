0eb7452c91a4131a37b380e0fce2b99b96112fd0
vinujk <vinu.jkataraki@gmail.com>
2025-04-03 02:01:12 -0400

    Add files via upload

--- socket1.c
+++ socket1.c
@@ -17,6 +17,8 @@ struct src_dst_ip *ip = NULL;
 
 struct session* path_head;
 struct session* resv_head;
+db_node *path_tree;
+db_node *resv_tree;
 
 int sock = 0;
 
@@ -24,6 +26,8 @@ int main() {
 
     char srcip[16];
     char dstip[16];
+    char nhip[16];
+    uint16_t tunnel_id;
     int explicit = 0;
     char buffer[512];
     struct sockaddr_in sender_addr;
@@ -61,6 +65,8 @@ int main() {
     printf("Enter dst ip: \n");
     fgets(dstip, 16, stdin);
 
+    printf("Enter tunnel_id: \n");
+    scanf("%hd",&tunnel_id);
     //printf("Is Explicit enable 1-yes 0-NO\n");
     //scanf("%d ", &explicit);
 
@@ -72,20 +78,46 @@ int main() {
     if(dstip[len-1] == '\n')
         dstip[len-1] = '\0';
 
+
+    path_msg *path = malloc(sizeof(path_msg));
+    path->tunnel_id = tunnel_id;
+    inet_pton(AF_INET, srcip, &path->src_ip);
+    inet_pton(AF_INET, dstip, &path->dest_ip);
+    //path->src_ip.s_addr = inet_addr("192.168.11.10");
+    //path->dest_ip.s_addr = inet_addr("192.168.11.11");
+
+    //get and assign nexthop
+    get_nexthop(inet_ntoa(path->dest_ip), nhip);
+    if(strcmp(nhip, " ") == 0)
+	inet_pton(AF_INET, "0.0.0.0", &path->nexthop_ip);
+    else 
+	inet_pton(AF_INET, nhip, &path->nexthop_ip);	
+
+    path->interval = 30;
+    path->setup_priority = 7;
+    path->hold_priority = 7;
+    path->flags = 0;
+    path->lsp_id = 1;
+    path->IFH = 123;
+    strncpy(path->name, "Path1", sizeof(path->name) - 1);
+    path->name[sizeof(path->name) - 1] = '\0';
+
+    path_tree = insert_node(path_tree, (void*)path, compare_path_insert); 
+    
     inet_pton(AF_INET, srcip, &send_ip);
     inet_pton(AF_INET, dstip, &rece_ip);
 	
     if(resv_head == NULL) {
-	resv_head = insert_session(resv_head, srcip, dstip, 1);
+	resv_head = insert_session(resv_head, tunnel_id, srcip, dstip, 1);
     } else {
-        insert_session(resv_head, srcip, dstip, 1);
+        insert_session(resv_head, tunnel_id, srcip, dstip, 1);
     }
 
     // Send RSVP-TE PATH Message
-    send_path_message(sock, send_ip, rece_ip, tunnel_id);
+    send_path_message(sock, send_ip, rece_ip, path->tunnel_id);
     //---------------------------------------------------------
 
-    //path_event_handler(); //send path msg
+    path_event_handler(); //send path msg
     int reached = 0;
 	 
     while(1) {
@@ -108,14 +140,14 @@ int main() {
 			resv_event_handler();
                         // get ip from the received path packet
 			printf(" in path msg type\n");
-			get_ip(buffer, sender_ip, receiver_ip);
+			get_ip(buffer, sender_ip, receiver_ip, &tunnel_id);
                         reached = dst_reached(sender_ip);
 
                         //printf("insert_path_session\n");
                         if(path_head == NULL) {
-                                path_head = insert_session(path_head, sender_ip, receiver_ip,reached);
+                                path_head = insert_session(path_head, tunnel_id, sender_ip, receiver_ip,reached);
                         } else {
-                                insert_session(path_head, sender_ip, receiver_ip, reached);
+                                insert_session(path_head, tunnel_id, sender_ip, receiver_ip, reached);
                         }
 
                         receive_path_message(sock,buffer,sender_addr);
@@ -129,14 +161,14 @@ int main() {
 
                         //get ip from the received resv msg
 			printf(" in resv msg type\n");
-                        get_ip(buffer, sender_ip, receiver_ip);
+                        get_ip(buffer, sender_ip, receiver_ip, &tunnel_id);
 			reached = dst_reached(sender_ip);
 
                         //printf("insert_resv_session\n");
                         if(resv_head == NULL) {
-                                resv_head = insert_session(resv_head, sender_ip, receiver_ip, reached);
+                                resv_head = insert_session(resv_head, tunnel_id, sender_ip, receiver_ip, reached);
                         } else {
-                                insert_session(resv_head, sender_ip, receiver_ip, reached);
+                                insert_session(resv_head, tunnel_id, sender_ip, receiver_ip, reached);
                         }
 
                         receive_resv_message(sock,buffer,sender_addr);
